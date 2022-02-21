032e4ba47d8f7f4e3b5775f8f17610a6c1a9e5c1
ROANVL <98936903+ROANVL@users.noreply.github.com>
2022-02-21 23:00:10 +0300

    Update dz3_2.py

--- dz3_2.py
+++ dz3_2.py
@@ -1,1 +1,139 @@
+persons = [
+        {'name': 'Alexander', 'age': 40, 'gender': 'male'},
+        {'name': 'Alexey', 'age': 16, 'gender': 'male'},
+        {'name': 'Anatoliy', 'age': 75, 'gender': 'male'},
+        {'name': 'Andrei', 'age': 10, 'gender': 'male'},
+        {'name': 'Arkadiy', 'age': 83, 'gender': 'male'},
+        {'name': 'Artyom', 'age': 13, 'gender': 'male'},
+        {'name': 'Danila', 'age': 72, 'gender': 'male'},
+        {'name': 'Dmitriy', 'age': 32, 'gender': 'male'},
+        {'name': 'Edward', 'age': 14, 'gender': 'male'},
+        {'name': 'Egor', 'age': 43, 'gender': 'male'},
+        {'name': 'Fyodor', 'age': 41, 'gender': 'male'},
+        {'name': 'Gennadiy', 'age': 66, 'gender': 'male'},
+        {'name': 'Georgiy', 'age': 59, 'gender': 'male'},
+        {'name': 'Grigoriy', 'age': 48, 'gender': 'male'},
+        {'name': 'Iakov', 'age': 73, 'gender': 'male'},
+        {'name': 'Ian', 'age': 64, 'gender': 'male'},
+        {'name': 'Innokentiy', 'age': 76, 'gender': 'male'},
+        {'name': 'Matvei', 'age': 53, 'gender': 'male'},
+        {'name': 'Maxim', 'age': 54, 'gender': 'male'},
+        {'name': 'Nikolai', 'age': 49, 'gender': 'male'},
+        {'name': 'Olesia', 'age': 63, 'gender': 'male'},
+        {'name': 'Petr', 'age': 87, 'gender': 'male'},
+        {'name': 'Philipp', 'age': 61, 'gender': 'male'},
+        {'name': 'Sergei', 'age': 27, 'gender': 'male'},
+        {'name': 'Sviatoslav', 'age': 9, 'gender': 'male'},
+        {'name': 'Timofei', 'age': 70, 'gender': 'male'},
+        {'name': 'Timour', 'age': 45, 'gender': 'male'},
+        {'name': 'Vasiliy', 'age': 78, 'gender': 'male'},
+        {'name': 'Viacheslav', 'age': 88, 'gender': 'male'},
+        {'name': 'Victor', 'age': 74, 'gender': 'male'},
+        {'name': 'Vitaliy', 'age': 20, 'gender': 'male'},
+        {'name': 'Yevgeniy', 'age': 22, 'gender': 'male'},
+        {'name': 'Yuriy', 'age': 84, 'gender': 'male'},
+        {'name': 'Zahar', 'age': 18, 'gender': 'male'},
+        {'name': 'Alexandra', 'age': 52, 'gender': 'female'},
+        {'name': 'Anastasiya', 'age': 29, 'gender': 'female'},
+        {'name': 'Angela', 'age': 21, 'gender': 'female'},
+        {'name': 'Clara', 'age': 62, 'gender': 'female'},
+        {'name': 'Daria', 'age': 35, 'gender': 'female'},
+        {'name': 'Ekaterina', 'age': 8, 'gender': 'female'},
+        {'name': 'Elena', 'age': 11, 'gender': 'female'},
+        {'name': 'Elizaveta', 'age': 65, 'gender': 'female'},
+        {'name': 'Evgenia', 'age': 85, 'gender': 'female'},
+        {'name': 'Ilia', 'age': 60, 'gender': 'female'},
+        {'name': 'Kseniya', 'age': 55, 'gender': 'female'},
+        {'name': 'Lidiya', 'age': 25, 'gender': 'female'},
+        {'name': 'Liliya', 'age': 7, 'gender': 'female'},
+        {'name': 'Liubov', 'age': 68, 'gender': 'female'},
+        {'name': 'Liudmila', 'age': 36, 'gender': 'female'},
+        {'name': 'Liya', 'age': 24, 'gender': 'female'},
+        {'name': 'Maia', 'age': 70, 'gender': 'female'},
+        {'name': 'Mariya', 'age': 45, 'gender': 'female'},
+        {'name': 'Natalia', 'age': 78, 'gender': 'female'},
+        {'name': 'Nataliya', 'age': 62, 'gender': 'female'},
+        {'name': 'Oxana', 'age': 35, 'gender': 'female'},
+        {'name': 'Sofia', 'age': 52, 'gender': 'female'},
+        {'name': 'Tatiana', 'age': 85, 'gender': 'female'},
+        {'name': 'Uliana', 'age': 15, 'gender': 'female'},
+        {'name': 'Valeriya', 'age': 55, 'gender': 'female'},
+        {'name': 'Veronica', 'age': 52, 'gender': 'female'},
+        {'name': 'Victoria', 'age': 36, 'gender': 'female'},
+        {'name': 'Viktoriya', 'age': 24, 'gender': 'female'},
+        {'name': 'Yevgeniya', 'age': 71, 'gender': 'female'},
+        {'name': 'Yuliya', 'age': 34, 'gender': 'female'},
+        {'name': 'Alexander', 'age': 40, 'gender': 'male'},
+        {'name': 'Anatoliy', 'age': 54, 'gender': 'male'},
+        {'name': 'Arkadiy', 'age': 49, 'gender': 'male'},
+        {'name': 'Artyom', 'age': 52, 'gender': 'male'},
+        {'name': 'Innokentiy', 'age': 17, 'gender': 'male'},
+        {'name': 'Matvei', 'age': 77, 'gender': 'male'},
+        {'name': 'Maxim', 'age': 89, 'gender': 'male'},
+        {'name': 'Nikolai', 'age': 60, 'gender': 'male'},
+        {'name': 'Timofei', 'age': 55, 'gender': 'male'},
+        {'name': 'Timour', 'age': 49, 'gender': 'male'},
+        {'name': 'Vasiliy', 'age': 70, 'gender': 'male'},
+        {'name': 'Clara', 'age': 78, 'gender': 'female'},
+        {'name': 'Daria', 'age': 62, 'gender': 'female'},
+        {'name': 'Alexandra', 'age': 78, 'gender': 'female'},
+        {'name': 'Evgenia', 'age': 85, 'gender': 'female'},
+        {'name': 'Elizaveta', 'age': 15, 'gender': 'female'},
+        {'name': 'Kseniya', 'age': 55, 'gender': 'female'},
+        {'name': 'Alexandra', 'age': 52, 'gender': 'female'},
+        {'name': 'Liudmila', 'age': 36, 'gender': 'female'},
+        {'name': 'Liya', 'age': 24, 'gender': 'female'},
+        {'name': 'Maia', 'age': 71, 'gender': 'female'},
+        {'name': 'Elizaveta', 'age': 72, 'gender': 'female'},
+        {'name': 'Alexander', 'age': 32, 'gender': 'male'},
+        {'name': 'Maxim', 'age': 14, 'gender': 'male'},
+        {'name': 'Nikolai', 'age': 43, 'gender': 'male'},
+        {'name': 'Alexandra', 'age': 41, 'gender': 'female'},
+        {'name': 'Yuliya', 'age': 66, 'gender': 'female'},
+        {'name': 'Uliana', 'age': 59, 'gender': 'female'},
+        {'name': 'Valeriya', 'age': 48, 'gender': 'female'},
+        {'name': 'Ilia', 'age': 73, 'gender': 'female'},
+        {'name': 'Kseniya', 'age': 64, 'gender': 'female'},
+        {'name': 'Nikolai', 'age': 76, 'gender': 'male'},
+        {'name': 'Timofei', 'age': 53, 'gender': 'male'},
+        {'name': 'Vasiliy', 'age': 54, 'gender': 'male'},
+        {'name': 'Clara', 'age': 49, 'gender': 'female'},
+        {'name': 'Vasiliy', 'age': 63, 'gender': 'male'},
+        ]
+
+#1. Количество всех людей
+print(f'1. Количество людей всего:', len(persons))
+
+#2. Сколько всего мужчин, а женщин
+men = 0
+women = 0
+for x in range(0, len(persons)):
+    if persons[x]['gender'] == 'female':
+        women += 1
+    elif persons[x]['gender'] == 'male':
+        men += 1   
+print(f'2. Мужчин всего: {men}, а женщин всего: {women}')
+
+
+#3. Сколько совершеннолетних
+v = 0
+for x in range(0, len(persons)):
+    if persons[x]['age'] >= int(18):
+        v += 1
+print(f'3. Совершеннолетних: ', v)
+
+
+#4. Список всех имен
+for item in persons:
+   print(item.get('name', None))
+   
+
+
+
+
+sorted_persons = sorted(persons, key=lambda d: d['name']) 
+for item in sorted_persons:
+   print(item.get('name', None))
+
+
 
